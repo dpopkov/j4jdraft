ab7b750eca4fa614380c690fa9a8b9f3d26cf0ee
Denis Popkov <pkvdenis@gmail.com>
2018-12-28 22:26:55 +0300

    collections: rename to SimpleHashMap, add comments, some improvements.

--- chapter_005/src/test/java/ru/j4jdraft/collections/map/SimpleHashMapTest.java
+++ chapter_005/src/test/java/ru/j4jdraft/collections/map/SimpleHashMapTest.java
@@ -2,16 +2,17 @@ package ru.j4jdraft.collections.map;
 
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
 
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
-public class HashMapTest {
+public class SimpleHashMapTest {
 
     @Test
     public void whenInsertKeyThenContainsKey() {
-        var map = new HashMap<Integer, String>(2);
+        var map = new SimpleHashMap<Integer, String>(2);
         assertThat(map.insert(1, "First"), is(true));
         assertThat(map.insert(-2, "Second"), is(true));
         assertThat(map.get(1), is("First"));
@@ -20,7 +21,7 @@ public class HashMapTest {
 
     @Test
     public void whenGetByOtherKeyWithSameHashcodeThenReturnNull() {
-        var map = new HashMap<FakeInt, String>(12);
+        var map = new SimpleHashMap<FakeInt, String>(12);
         FakeInt key1 = FakeInt.of(1);
         FakeInt key2 = FakeInt.of(11);
         assertNotEquals(key1, key2);
@@ -32,7 +33,7 @@ public class HashMapTest {
 
     @Test
     public void whenInsertExistingKeyThenContainsOldKeyValuePair() {
-        var map = new HashMap<Integer, String>(2);
+        var map = new SimpleHashMap<Integer, String>(2);
         assertThat(map.insert(11, "old eleven"), is(true));
         assertThat(map.insert(11, "new eleven"), is(false));
         assertThat(map.get(11), is("old eleven"));
@@ -40,7 +41,7 @@ public class HashMapTest {
 
     @Test
     public void whenGetByNonExistingKeyThenReturnNull() {
-        var map = new HashMap<Integer, String>(16);
+        var map = new SimpleHashMap<Integer, String>(16);
         assertThat(map.insert(11, "eleven"), is(true));
         assertThat(map.insert(12, "twelve"), is(true));
         assertNull(map.get(10));
@@ -48,7 +49,7 @@ public class HashMapTest {
 
     @Test
     public void whenInsertInFullTableThenContainsAllKeys() {
-        var map = new HashMap<Integer, String>(0);
+        var map = new SimpleHashMap<Integer, String>(0);
         map.insert(1, "one");
         map.insert(2, "two");
         map.insert(3, "three");
@@ -63,7 +64,7 @@ public class HashMapTest {
 
     @Test
     public void whenDeleteKeyThenDoesNotContainKey() {
-        var map = new HashMap<Integer, String>(2);
+        var map = new SimpleHashMap<Integer, String>(2);
         map.insert(1, "one");
         map.insert(2, "two");
         assertThat(map.get(1), is("one"));
@@ -79,20 +80,20 @@ public class HashMapTest {
 
     @Test
     public void whenDeleteNonExistingKeyThenReturnsFalse() {
-        var map = new HashMap<Integer, String>(2);
+        var map = new SimpleHashMap<Integer, String>(2);
         map.insert(1, "one");
         assertThat(map.delete(11), is(false));
     }
 
     @Test(expected = NoSuchElementException.class)
     public void whenIterateEmptyThenNoSuchElementException() {
-        var map = new HashMap<Integer, String>(0);
+        var map = new SimpleHashMap<Integer, String>(0);
         map.iterator().next();
     }
 
     @Test
     public void whenIterateThenRetrievesAllEntries() {
-        var map = new HashMap<Integer, String>(22);
+        var map = new SimpleHashMap<Integer, String>(22);
         map.insert(1, "one");
         map.insert(21, "twenty one");
         var it = map.iterator();
@@ -104,5 +105,13 @@ public class HashMapTest {
         assertThat(entry.getKey(), is(21));
         assertThat(entry.getValue(), is("twenty one"));
         assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIterateAfterModificationThenException() {
+        var map = new SimpleHashMap<Integer, String>(2);
+        var it = map.iterator();
+        map.insert(1, "one");
+        it.next();
     }
 }
