28deed4c40c94aa328db844838a32e0e92a9073e
Denis Popkov <pkvdenis@gmail.com>
2019-02-19 17:32:16 +0300

    io: ZipFolder - refactor and wrap IOException.

--- chapter_006/src/main/java/ru/j4jdraft/io/zip/ZipFolder.java
+++ chapter_006/src/main/java/ru/j4jdraft/io/zip/ZipFolder.java
@@ -39,20 +39,43 @@ public class ZipFolder {
     public Path compress(Path directory, Path output, List<String> excludeExt) throws IOException {
         FileExtensionFilter exclude = new FileExtensionFilter(excludeExt);
         try (ZipOutputStream out = new ZipOutputStream(Files.newOutputStream(output))) {
-            Files.walk(directory).forEach(entry -> {
-                if (Files.isRegularFile(entry) && (excludeExt == null || exclude.reject(entry))) {
-                    Path sourcePath = directory.relativize(entry);
-                    try {
-                        out.putNextEntry(new ZipEntry(sourcePath.toString()));
-                        byte[] bytes = Files.readAllBytes(entry);
-                        out.write(bytes, 0, bytes.length);
-                        out.closeEntry();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            try {
+                Files.walk(directory)
+                        .filter(entry -> Files.isRegularFile(entry) && (excludeExt == null || exclude.reject(entry)))
+                        .forEach(entry -> writeEntryToZip(out, directory, entry));
+            } catch (WrappedIOException e) {
+                if (e.getCause() instanceof IOException) {
+                    throw (IOException) e.getCause();
                 }
-            });
+                throw e;
+            }
         }
         return output;
+    }
+
+    /**
+     * Writes the specified entry to zip output stream.
+     * @param out output to zip
+     * @param baseDir base directory, all files of which are archived
+     * @param entry path to every file that should be zipped
+     * @throws WrappedIOException if an I/O error has occurred this exception saves the cause
+     */
+    private void writeEntryToZip(ZipOutputStream out, Path baseDir, Path entry) {
+        try {
+            out.putNextEntry(new ZipEntry(baseDir.relativize(entry).toString()));
+            out.write(Files.readAllBytes(entry));
+            out.closeEntry();
+        } catch (IOException e) {
+            throw new WrappedIOException(e);
+        }
+    }
+
+    /**
+     * Runtime exception wrapper for {@code IOException}.
+     */
+    public static class WrappedIOException extends RuntimeException {
+        public WrappedIOException(IOException cause) {
+            super(cause);
+        }
     }
 }
