bd7951218cab1c053357e1a21a6e6080f4eb322f
Denis Popkov <pkvdenis@gmail.com>
2019-03-14 20:20:14 +0300

    io: find: refactor ArgsTest.

--- find/src/test/java/ru/j4jdraft/io/find/ArgsTest.java
+++ find/src/test/java/ru/j4jdraft/io/find/ArgsTest.java
@@ -2,7 +2,6 @@ package ru.j4jdraft.io.find;
 
 import org.junit.Test;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.hamcrest.core.Is.*;
@@ -12,39 +11,33 @@ public class ArgsTest {
 
     @Test
     public void whenArgsContainAllValuesThenReturnAllArgumentValues() {
-        String[] arguments = splitArguments("-d c:\\ -n *.txt -m -o result.txt");
-        Args args = new Args(arguments);
-        assertThat(args.getDirectory(), is(Paths.get("c:\\")));
-        assertThat(args.getName(), is("*.txt"));
-        assertThat(args.getSearchBy(), is(SearchBy.MASK));
-        assertThat(args.getOutput(), is("result.txt"));
+        Args args = new Args(splitArguments("-d c:\\ -n *.txt -m -o result.txt"));
+        assertArgs(args, "c:\\", "*.txt", SearchBy.MASK, "result.txt");
     }
 
     @Test
     public void whenArgsContainAllValuesInRandomOrderThenReturnAllArgumentValues() {
-        String[] arguments = splitArguments("-r -n [a-z]+\\d?.\\w{2,4} -o result.txt -d c:\\test");
-        Args args = new Args(arguments);
-        assertThat(args.getDirectory(), is(Paths.get("c:\\test")));
-        assertThat(args.getName(), is("[a-z]+\\d?.\\w{2,4}"));
-        assertThat(args.getSearchBy(), is(SearchBy.REGEX));
-        assertThat(args.getOutput(), is("result.txt"));
+        Args args = new Args(splitArguments("-r -n [a-z]+\\d?.\\w{2,4} -o result.txt -d c:\\test"));
+        assertArgs(args, "c:\\test", "[a-z]+\\d?.\\w{2,4}", SearchBy.REGEX, "result.txt");
     }
 
     @Test
     public void whenArgsDoNotContainAllValuesThenReturnDefaultArgumentValues() {
-        String[] arguments = splitArguments("-n *.txt");
-        Args args = new Args(arguments);
-        final Path currentDirectory = Paths.get("");
-        assertThat(args.getDirectory(), is(currentDirectory));
-        assertThat(args.getName(), is("*.txt"));
-        assertThat(args.getSearchBy(), is(SearchBy.FULL));
-        assertThat(args.getOutput(), is("log.txt"));
+        Args args = new Args(splitArguments("-n *.txt"));
+        assertArgs(args, System.getProperty("user.dir"), "*.txt", SearchBy.FULL, "log.txt");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenArgsWithNonCorrectArgumentsThenThrowException() {
-        String[] arguments = splitArguments("-z result.txt");
-        new Args(arguments);
+        new Args(splitArguments("-z result.txt"));
+    }
+
+    private void assertArgs(Args args, String directory, String name, SearchBy searchBy, String output) {
+        assertThat(args.getDirectory().toAbsolutePath().normalize(),
+                is(Paths.get(directory).toAbsolutePath().normalize()));
+        assertThat(args.getName(), is(name));
+        assertThat(args.getSearchBy(), is(searchBy));
+        assertThat(args.getOutput(), is(output));
     }
 
     private String[] splitArguments(String args) {
