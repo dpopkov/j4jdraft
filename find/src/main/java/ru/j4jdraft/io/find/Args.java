08c13d449fb32f4dc4991dc3d5576a59d84e28f1
Denis Popkov <pkvdenis@gmail.com>
2019-03-14 18:49:56 +0300

    io: find: refactor Args, add SearchBy.

--- find/src/main/java/ru/j4jdraft/io/find/Args.java
+++ find/src/main/java/ru/j4jdraft/io/find/Args.java
@@ -2,34 +2,41 @@ package ru.j4jdraft.io.find;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.*;
 
 public class Args {
     private static final String DEFAULT_OUTPUT = "log.txt";
-
+    /** Command line arguments. */
+    private String[] args;
+    /** Index of current argument. */
+    private int idx;
     private Path directory = Paths.get("");
     /** Name of file or search pattern. */
     private String name;
     private SearchBy searchBy = SearchBy.FULL;
     private String output = DEFAULT_OUTPUT;
 
+    private final Map<String, Supplier<Object>> actions = Map.of(
+            "-d", () -> directory = Paths.get(args[++idx]),
+            "-n", () -> name = args[++idx],
+            "-o", () -> output = args[++idx],
+            "-m", () -> searchBy = SearchBy.MASK,
+            "-f", () -> searchBy = SearchBy.FULL,
+            "-r", () -> searchBy = SearchBy.REGEX
+    );
+
     public Args(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            String value = args[i];
-            if ("-d".equals(value)) {
-                directory = Paths.get(args[++i]);
-            } else if ("-n".equals(value)) {
-                name = args[++i];
-            } else if ("-o".equals(value)) {
-                output = args[++i];
-            } else if ("-m".equals(value)) {
-                searchBy = SearchBy.MASK;
-            } else if ("-f".equals(value)) {
-                searchBy = SearchBy.FULL;
-            } else if ("-r".equals(value)) {
-                searchBy = SearchBy.REGEX;
-            } else {
-                throw new IllegalArgumentException(String.format("Invalid argument %s", value));
-            }
+        this.args = args;
+        parse();
+    }
+
+    private void parse() {
+        for (idx = 0; idx < args.length; idx++) {
+            String arg = args[idx];
+            actions.getOrDefault(arg, () -> {
+                throw new IllegalArgumentException(String.format("Invalid argument: '%s'", arg));
+            }).get();
         }
     }
 
---
+++ find/src/main/java/ru/j4jdraft/io/find/SearchBy.java
@@ -0,0 +1,11 @@
+package ru.j4jdraft.io.find;
+
+/** Type of search. */
+public enum SearchBy {
+    /** Search by mask. */
+    MASK,
+    /** Search by full name. */
+    FULL,
+    /** Search using regular expression. */
+    REGEX
+}
