d39eec37b578eb45f718c1a498da47c41f15625e
Denis Popkov <pkvdenis@gmail.com>
2019-06-17 01:35:59 +0300

    sql: JdbcHelper - make methods non-static, add tests.

--- chapter_007_db/src/main/java/ru/j4jdraft/tracker/JdbcHelper.java
+++ chapter_007_db/src/main/java/ru/j4jdraft/tracker/JdbcHelper.java
@@ -9,62 +9,115 @@ import java.sql.*;
 import java.util.List;
 import java.util.Properties;
 
-public class JdbcHelper implements AutoCloseable {
+/**
+ * Contains helper methods to create, populate and open connection to a database.
+ */
+public class JdbcHelper {
+    public static final String PROPERTIES_FILENAME = "db.properties";
+    public static final String CHECK_DB_SQL = "SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower(?)";
+
     private static final Logger LOG = LoggerFactory.getLogger(JdbcHelper.class);
-    private static final String PROPERTIES_FILENAME = "db.properties";
-    private static final String BASE_URL = "jdbc:postgresql://localhost:5432/";
-    private static final String POSTGRES_URL = BASE_URL + "postgres";
-    private Connection connection;
 
-    public JdbcHelper(Properties properties) throws SQLException {
-        connection = DriverManager.getConnection("url", properties);
+    private final Properties properties;
+    private final String baseUrl;
+    private final String postgresUrl;
+
+    /**
+     * Constructs and initializes this helper with the specified properties.
+     * @param properties properties containing these keys: baseUrl, user, password
+     */
+    public JdbcHelper(Properties properties) {
+        this.properties = properties;
+        baseUrl = properties.getProperty("baseUrl");
+        postgresUrl = baseUrl + "postgres";
     }
 
-    /** Returns true if the db exists already, false if the db was created. */
-    public static boolean ensureDbExists(Properties dbProperties, String dbName, String scriptName) throws SQLException, IOException {
-        final String checkDbSql = "SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower(?)";
-        boolean exists;
-        try (Connection pgConnection = DriverManager.getConnection(POSTGRES_URL, dbProperties)) {
-            PreparedStatement ps = pgConnection.prepareStatement(checkDbSql);
-            ps.setString(1, dbName);
-            ResultSet result = ps.executeQuery();
-            exists = result.next();
-            if (!exists) {
-                final String createDbSql = "create database " + dbName;
-                Statement st = pgConnection.createStatement();
-                st.execute(createDbSql);
+    /**
+     * Checks if the specified database exists.
+     * @param pgConnection connection to the postgres database
+     * @param dbName name of the database
+     * @return true if db exists, false otherwise
+     * @throws SQLException if a database access error occurs
+     */
+    public boolean dbExists(Connection pgConnection, String dbName) throws SQLException {
+        PreparedStatement ps = pgConnection.prepareStatement(CHECK_DB_SQL);
+        ps.setString(1, dbName);
+        ResultSet result = ps.executeQuery();
+        return result.next();
+    }
+
+    /**
+     * Creates database with the specified name.
+     * @param pgConnection connection to the postgres database
+     * @param dbName name of the database
+     * @throws SQLException if a database access error occurs
+     */
+    public void createDb(Connection pgConnection, String dbName) throws SQLException {
+        String createDbSql = "create database " + dbName;
+        Statement st = pgConnection.createStatement();
+        st.execute(createDbSql);
+    }
+
+    /**
+     * Populates the specified database with data.
+     * @param dbConnection connection to the database
+     * @param sqlStatements sql statements to populate the database
+     */
+    public void populateDb(Connection dbConnection, List<String> sqlStatements) throws SQLException {
+        Statement st = dbConnection.createStatement();
+        for (String sql : sqlStatements) {
+            st.addBatch(sql);
+        }
+        st.executeBatch();
+    }
+
+    /**
+     * Ensures that the specified database exists, or creates and fills it with data.
+     * @param dbName name of the database
+     * @param scriptName name of the script containing sql statements to insert data to the database
+     * @throws SQLException if a database access error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public void ensureDbExists(String dbName, String scriptName) throws SQLException, IOException {
+        try (Connection pgConnection = DriverManager.getConnection(postgresUrl, properties)) {
+            if (dbExists(pgConnection, dbName)) {
+                return;
+            } else {
+                createDb(pgConnection, dbName);
             }
         }
-        if (exists) {
-            return true;
-        } else {
-            List<String> sqlList = ResourceReader.readSqlStatements(scriptName);
-            try (Connection connection = DriverManager.getConnection(BASE_URL + dbName, dbProperties)) {
-                Statement st = connection.createStatement();
-                for (String sql : sqlList) {
-                    st.addBatch(sql);
-                }
-                st.executeBatch();
+        try (InputStream in = JdbcHelper.class.getClassLoader().getResourceAsStream(scriptName);
+             Connection dbConnection = DriverManager.getConnection(baseUrl + dbName, properties)) {
+            List<String> sqlList = new SqlReader().readSqlStatements(in);
+            populateDb(dbConnection, sqlList);
+        }
+    }
+
+    /**
+     * Ensures that the specified database exists, or creates and fills it with data,
+     * then opens the connection to the database.
+     * @param dbName database name
+     * @param scriptName name of the script containing sql statements to insert data to the database
+     * @return connection to existing database
+     * @throws SQLException if a database access error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public Connection connectToExistingDb(String dbName, String scriptName) throws SQLException, IOException {
+        boolean populate = false;
+        try (Connection pgConnection = DriverManager.getConnection(postgresUrl, properties)) {
+            if (!dbExists(pgConnection, dbName)) {
+                createDb(pgConnection, dbName);
+                populate = true;
             }
-            return false;
         }
-    }
-
-    public static boolean createDb(Properties dbProperties, String dbName) throws SQLException {
-        final String createDbSql = "create database " + dbName;
-        boolean created;
-        try (Connection pgConnection = DriverManager.getConnection(POSTGRES_URL, dbProperties)) {
-            Statement st = pgConnection.createStatement();
-            created = st.execute(createDbSql);
+        Connection dbConnection = DriverManager.getConnection(baseUrl + dbName, properties);
+        if (populate) {
+            try (InputStream in = JdbcHelper.class.getClassLoader().getResourceAsStream(scriptName)) {
+                List<String> sqlList = new SqlReader().readSqlStatements(in);
+                populateDb(dbConnection, sqlList);
+            }
         }
-        return created;
-    }
-
-    @Override
-    public void close() throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
-        }
+        return dbConnection;
     }
 
     public static Properties readProperties(String filename) {
@@ -91,12 +144,9 @@ public class JdbcHelper implements AutoCloseable {
         }
         Properties props = readProperties(PROPERTIES_FILENAME);
         try {
-            boolean existed = ensureDbExists(props, dbName, "sql/createTestDb1.sql" );
-            if (existed) {
-                System.out.println("Db exists already");
-            } else {
-                System.out.println("Creation finished");
-            }
+            JdbcHelper helper = new JdbcHelper(props);
+            helper.ensureDbExists(dbName, "sql/createTestDb1.sql" );
+            System.out.println("Finished");
         } catch (SQLException | IOException e) {
             LOG.error(e.getMessage(), e);
         }
---
+++ chapter_007_db/src/test/java/ru/j4jdraft/tracker/JdbcHelperTest.java
@@ -0,0 +1,50 @@
+package ru.j4jdraft.tracker;
+
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Properties;
+
+import static org.mockito.Mockito.*;
+
+public class JdbcHelperTest {
+
+    @Test
+    public void testDbExists() throws SQLException {
+        Connection conn = mock(Connection.class);
+        PreparedStatement ps = mock(PreparedStatement.class);
+        ResultSet rs = mock(ResultSet.class);
+        when(conn.prepareStatement(anyString())).thenReturn(ps);
+        when(ps.executeQuery()).thenReturn(rs);
+
+        JdbcHelper helper = new JdbcHelper(mock(Properties.class));
+        helper.dbExists(conn, "database_name");
+        verify(conn).prepareStatement(JdbcHelper.CHECK_DB_SQL);
+        verify(ps).setString(1, "database_name");
+    }
+
+    @Test
+    public void testCreateDb() throws SQLException {
+        Connection conn = mock(Connection.class);
+        Statement st = mock(Statement.class);
+        when(conn.createStatement()).thenReturn(st);
+
+        JdbcHelper helper = new JdbcHelper(mock(Properties.class));
+        helper.createDb(conn, "database_name");
+        verify(st).execute("create database database_name");
+    }
+
+    @Test
+    public void testPopulateDb() throws SQLException {
+        Connection conn = mock(Connection.class);
+        Statement stmt = mock(Statement.class);
+        when(conn.createStatement()).thenReturn(stmt);
+
+        JdbcHelper helper = new JdbcHelper(mock(Properties.class));
+        List<String> sqlList = List.of("statement1", "statement2");
+        helper.populateDb(conn, sqlList);
+        verify(stmt, times(2)).addBatch(anyString());
+        verify(stmt).executeBatch();
+    }
+}
