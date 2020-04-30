e85e35070577f4cbf706f1c750e5a2b7c863f18f
Denis Popkov <pkvdenis@gmail.com>
2020-04-30 20:20:51 +0300

    titcat, add comments to Config

--- chapter_008/src/main/java/ru/j4jdraft/ood/tictac/model/Config.java
+++ chapter_008/src/main/java/ru/j4jdraft/ood/tictac/model/Config.java
@@ -1,9 +1,21 @@
 package ru.j4jdraft.ood.tictac.model;
 
 public interface Config {
+    /** Returns type of user interface. */
     String uiType();
+
+    /** Returns size of the square grid. */
     int gridSize();
+
+    /** Returns id of the player that should start the game. */
     int getStartingId();
+
+    /**
+     * Returns number of adjacent marks that a winner
+     * should place in horizontal, vertical, or diagonal row.
+     */
     int getWinLineLength();
+
+    /** Returns true if this config is initialized. */
     boolean isInitialized();
 }
